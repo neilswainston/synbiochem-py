86ccc7abe4b7969bcd9d31ecc8b7bf99c7f2f21d
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-11-18 20:40:17 +0000

    Added tests for linear programming method.

--- synbiochem/utils/math_utils.py
+++ synbiochem/utils/math_utils.py
@@ -7,30 +7,30 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
-# pylint: disable=invalid-name
 # pylint: disable=no-member
 import gurobipy
 
 
-def isclose(aaa, bbb, rel_tol=1e-09, abs_tol=0.0):
+def isclose(value_1, value_2, rel_tol=1e-09, abs_tol=0.0):
     '''Compares floating point numbers.'''
-    return abs(aaa - bbb) <= max(rel_tol * max(abs(aaa), abs(bbb)), abs_tol)
+    return abs(value_1 - value_2) <= max(rel_tol * max(abs(value_1),
+                                                       abs(value_2)), abs_tol)
 
 
-def linprog(ccc, A_eq, b_eq, bounds):
+def linprog(c_vector, a_eq, b_eq, bounds):
     '''Solve linear programming problem.'''
     model = gurobipy.Model()
     model.Params.OutputFlag = 0  # Silent mode
 
     # Create variables:
-    for obj, bound in zip(ccc, bounds):
+    for obj, bound in zip(c_vector, bounds):
         model.addVar(bound[0], bound[1], obj, gurobipy.GRB.CONTINUOUS)
 
     # Integrate new variables:
     model.update()
 
     # Add constraints:
-    for aaa, bbb in zip(A_eq, b_eq):
+    for aaa, bbb in zip(a_eq, b_eq):
         model.addConstr(gurobipy.LinExpr(aaa, model.getVars()),
                         gurobipy.GRB.EQUAL,
                         bbb)
--- synbiochem/utils/test/test_math_utils.py
+++ synbiochem/utils/test/test_math_utils.py
@@ -21,6 +21,21 @@ class Test(unittest.TestCase):
         self.assertTrue(math_utils.isclose(18.015054684, 18.015054684))
         self.assertFalse(math_utils.isclose(18.015054684, 18.015054))
 
+    def test_linprog(self):
+        '''Tests linprog method.'''
+        a_matrix = [[-238, -142, 1, 248, 131, -1, -2, 1, 2],
+                    [-144, -86, 0, 150, 80, 0, 0, 0, 0],
+                    [-2, 0, 0, 2, 0, 0, 0, 0, 0],
+                    [-57, -9, 0, 62, 4, 0, -1, 0, 1],
+                    [-2, -1, 0, 2, 1, 0, 0, 0, 0],
+                    [2.0, 1.0, 1.0, -2.0, -2.0, -1, 0, 1, 0]]
+
+        bounds = [(1, 8), (1, 8), (1, 8), (1, 8), (1, 8), (0, 8), (0, 8),
+                  (0, 8), (0, 8)]
+
+        self.assertTrue(math_utils.linprog([1] * len(a_matrix[0]), a_matrix,
+                                           [0] * len(a_matrix), bounds)[0])
+
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
