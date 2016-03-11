271c25814ca59b92e54782e2533cc8dd7325f5d1
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-03-11 20:30:12 +0000

    Interim check-in of ICE code.

--- synbiochem/utils/ice_utils.py
+++ synbiochem/utils/ice_utils.py
@@ -8,13 +8,55 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 @author:  pablocarbonell / neilswainston / alanwilliams
 '''
 # pylint: disable=too-many-arguments
+
 import copy
 import json
 import tempfile
 import urllib
 
 from synbiochem.utils import net_utils as net_utils
-from synbiochem.utils import sbol_utils as sbol_utils
+from synbiochem.utils.sbol_utils import SBOLDocument
+
+
+class ICEEntry(object):
+    '''Class to represent an ICE entry.'''
+
+    def __init__(self, sbol_doc=None, typ=None, metadata=None):
+
+        assert typ is not None or metadata is not None
+
+        if sbol_doc is None:
+            self.__sbol_doc = SBOLDocument()
+        else:
+            self.__sbol_doc = sbol_doc
+
+        if metadata is None:
+            self.__metadata = {'type': typ}
+        else:
+            self.__metadata = metadata
+
+    def get_ice_number(self):
+        '''Gets the ICE id.'''
+        return self.__metadata['id'] if 'id' in self.__metadata else None
+
+    def get_metadata(self):
+        '''Gets the metadata.'''
+        return self.__metadata
+
+    def get_sbol_doc(self):
+        '''Gets the SBOLDocument.'''
+        return self.__sbol_doc
+
+    def set_values(self, new_metadata):
+        '''Sets multiple metadata values.'''
+        self.__metadata.update(new_metadata)
+
+    def set_value(self, key, value):
+        '''Sets a metadata value.'''
+        self.__metadata[key] = value
+
+    def __repr__(self):
+        return str(self.__metadata) + '\n' + str(self.__sbol_doc)
 
 
 class ICEClient(object):
@@ -34,122 +76,64 @@ class ICEClient(object):
             'X-ICE-Authentication-SessionId'] = self.__sid
         self.__id_prefix = id_prefix
 
-    def get_meta_data(self, part_id):
+    def get_ice_entry(self, ice_id):
+        '''Gets an ICEEntry object from the ICE database.'''
+        return ICEEntry(sbol_doc=self.__get_sbol_doc(ice_id),
+                        metadata=self.__get_meta_data(ice_id))
+
+    def set_ice_entry(self, ice_entry):
+        '''Saves an ICEEntry object in the ICE database.'''
+        if ice_entry.get_ice_number() is None:
+            self.__create_entry(ice_entry)
+        else:
+            self.__update_entry(ice_entry.get_ice_number(),
+                                ice_entry.get_metadata())
+
+        self.__upload_sbol(ice_entry.get_ice_number(),
+                           ice_entry.get_sbol_doc())
+
+    def __get_meta_data(self, ice_id):
         '''Returns an ICE entry metadata.'''
         return _read_resp(net_utils.get(
-            self.__url + '/parts/' + self.__get_part_number(part_id),
+            self.__url + '/parts/' + self.__get_ice_number(ice_id),
             self.__headers))
 
-    def get_sequence(self, part_id):
+    def __get_sbol_doc(self, ice_id):
         '''Gets the sequence ICE entry.'''
-        url = self.__url + '/file/' + self.__get_part_number(part_id) + \
+        url = self.__url + '/file/' + self.__get_ice_number(ice_id) + \
             '/sequence/sbol?sid=' + self.__sid
         temp_file = tempfile.NamedTemporaryFile(delete=False)
         urllib.urlretrieve(url, temp_file.name)
-        return sbol_utils.SbolDocument(temp_file.name).get_dna_sequence()
+        return SBOLDocument(temp_file.name)
 
-    def create_part(self, data):
-        '''Creates a new ICE part.'''
+    def __create_entry(self, ice_entry):
+        '''Creates a new ICE entry in the database.'''
         url = self.__url + '/parts'
-        return _read_resp(
-            net_utils.post(url, json.dumps(data), self.__headers))
+        response = _read_resp(
+            net_utils.post(url, json.dumps(ice_entry.get_metadata()),
+                           self.__headers))
 
-    def set_sequence(self, part_number, data):
-        '''Sets an ICE part sequence.'''
-        url = self.__url + '/parts/' + str(part_number) + '/sequence'
-        return _read_resp(
-            net_utils.post(url, json.dumps(data), self.__headers))
+        ice_entry.set_values(response)
 
-    def update_part(self, part_number, data):
-        '''Updates an ICE part.'''
-        url = self.__url + '/parts/' + str(part_number)
-        return _read_resp(net_utils.put(url, json.dumps(data), self.__headers))
+    def __update_entry(self, ice_id, metadata):
+        '''Updates an ICE entry in the database.'''
+        ice_number = self.__get_ice_number(ice_id)
+        url = self.__url + '/parts/' + str(ice_number)
+        _read_resp(net_utils.put(url, json.dumps(metadata), self.__headers))
 
-    # def get_sequence(self, part_number):
-    #    '''Gets an ICE part sequence.'''
-    #    url = self.__url + '/parts/' + str(part_number) + '/sequence'
-    #    return _read_resp(net_utils.get(url, self.__headers))
-
-    def upload_sequence(self, part_number, seqfile):
-        '''Uploads an annotated sequence.'''
-        url = self.__url + '/uploads/' + str(part_number) + '/sequence'
-        files = {'file': open(seqfile, 'rb'), 'entryId': str(part_number)}
+    def __upload_sbol(self, ice_number, sbol_doc):
+        '''Uploads an SBOLDocument to ICE database.'''
+        url = self.__url + '/file/sequence'
+        files = {'file': str(sbol_doc), 'entryRecordId': str(ice_number)}
         return _read_resp(net_utils.post(url, files, self.__headers))
 
-    def __get_part_number(self, part_id):
-        '''Maps part number to part id, i.e. from SBC000123 to 123.'''
-        return str(int(part_id.replace(self.__id_prefix, '')))
+    def __get_ice_number(self, ice_id):
+        '''Maps ICE number to ICE id, i.e. from SBC000123 to 123.'''
+        return str(int(ice_id.replace(self.__id_prefix, '')))
 
-
-def get_template_newplasmid(name, description, creator, owner, investigator,
-                            creator_email=None, owner_email=None,
-                            investigator_email=None):
-    '''Gets the template for a new ICE plasmid.'''
-    return {u'accessPermissions': [],
-            u'basePairCount': 0,
-            u'bioSafetyLevel': 1,
-            u'canEdit': True,
-            u'creator': creator,
-            u'creatorEmail': creator_email,
-            u'featureCount': 0,
-            u'fundingSource': u'BBSRC',
-            u'hasAttachment': False,
-            u'hasOriginalSequence': False,
-            u'hasSample': False,
-            u'hasSequence': False,
-            u'index': 0,
-            u'linkedParts': [],
-            u'links': [],
-            u'name': name,
-            u'owner': owner,
-            u'ownerEmail': owner_email,
-            u'parameters': [],
-            u'parents': [],
-            u'plasmidData': {u'circular': True},
-            u'principalInvestigator': investigator,
-            u'principalInvestigatorEmail': investigator_email,
-            u'publicRead': False,
-            u'selectionMarkers': [],
-            u'shortDescription': description,
-            u'status': u'Planned',
-            u'type': u'PLASMID',
-            u'visible': u'OK'
-            }
-
-
-def template_newpart(name, description, creator, owner, investigator,
-                     creator_email=None, owner_email=None,
-                     investigator_email=None):
-    '''Gets the template for a new ICE part.'''
-    return {u'accessPermissions': [],
-            u'basePairCount': 0,
-            u'bioSafetyLevel': 1,
-            u'canEdit': True,
-            u'creator': creator,
-            u'creatorEmail': creator_email,
-            u'featureCount': 0,
-            u'fundingSource': u'BBSRC',
-            u'hasAttachment': False,
-            u'hasOriginalSequence': False,
-            u'hasSample': False,
-            u'hasSequence': False,
-            u'index': 0,
-            u'linkedParts': [],
-            u'links': [],
-            u'name': name,
-            u'owner': owner,
-            u'ownerEmail': owner_email,
-            u'parameters': [],
-            u'parents': [],
-            u'principalInvestigator': investigator,
-            u'principalInvestigatorEmail': investigator_email,
-            u'publicRead': False,
-            u'selectionMarkers': [],
-            u'shortDescription': description,
-            u'status': u'Complete',
-            u'type': u'PART',
-            u'visible': u'OK'
-            }
+    def __get_ice_id(self, ice_number):
+        '''Maps ICE id to ICE number, i.e. from 123 to SBC000123.'''
+        return self.__id_prefix + format(ice_number, '06')
 
 
 def _read_resp(response):
@@ -177,10 +161,16 @@ def main():
     username = 'Administrator'
     password = 'synbiochem'
 
-    ice = ICEClient(url, username, password)
-    print ice.get_meta_data('SBC00008')
-    print ice.get_sequence('SBC8')
+    ice = ICEClient(url, username, password, id_prefix='TEST_')
+    print ice.get_ice_entry('TEST_000008').get_sbol_doc().get_sequence()
 
+    ice_entry = ICEEntry(typ='PART')
+    print ice_entry.get_sbol_doc().get_sequence()
+    ice.set_ice_entry(ice_entry)
+    print ice.get_ice_entry(ice_entry.get_ice_number())
+    ice_entry.set_value('owner', 'Fabrizio Ravanelli')
+    ice.set_ice_entry(ice_entry)
+    print ice.get_ice_entry(ice_entry.get_id())
 
 if __name__ == '__main__':
     main()
--- synbiochem/utils/sbol_utils.py
+++ synbiochem/utils/sbol_utils.py
@@ -5,27 +5,60 @@ synbiochem is licensed under the MIT License.
 
 To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
-@author:  neilswainston
+@author:  pablocarbonell / neilswainston / alanwilliams
 '''
 from xml.etree import ElementTree
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+_PATH_TO_SEQ = './sbol:DnaComponent/sbol:dnaSequence/sbol:DnaSequence' + \
+    '/sbol:nucleotides'
 
 
-class SbolDocument(object):
-    '''Simple, lightweight SBOL v1.1 reader / writer?'''
+class SBOLDocument(object):
+    '''Class representing an SBOL Document.'''
 
-    def __init__(self, filename):
-        with open(filename) as fle:
-            self.__root = ElementTree.parse(fle).getroot()
-
+    def __init__(self, filename=None):
         self.__ns = {'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
                      'sbol': 'http://sbols.org/v1#'}
 
-    def get_dna_sequence(self):
+        for prefix, uri in self.__ns.iteritems():
+            ElementTree.register_namespace(prefix, uri)
+
+        if filename is None:
+            self.__root = Element(self.__get_ns('rdf') + 'RDF')
+            self.__init_sequence()
+        else:
+            with open(filename) as fle:
+                self.__root = ElementTree.parse(fle).getroot()
+
+    def get_sequence(self):
         '''Gets the DNA sequence.'''
-        path = './sbol:DnaComponent/sbol:dnaSequence/sbol:DnaSequence' + \
-            '/sbol:nucleotides'
-        elements = self.__root.findall(path, self.__ns)
-        return elements[0].text.upper()
+        return self.__get_seq_element().text.upper()
+
+    def set_sequence(self, seq):
+        '''Sets the DNA sequence.'''
+        return self.__get_seq_element().set_text(seq)
+
+    def __init_sequence(self, seq=''):
+        '''Initialises a SBOLDocument.'''
+        sbolns = self.__get_ns('sbol')
+        child_elem = SubElement(self.__root, sbolns + 'DnaComponent')
+        child_elem = SubElement(child_elem, sbolns + 'dnaSequence')
+        child_elem = SubElement(child_elem, sbolns + 'DnaSequence')
+        child_elem = SubElement(child_elem, sbolns + 'nucleotides')
+        child_elem.text = seq
+
+    def __get_seq_element(self):
+        '''Gets the DNA sequence element.'''
+        elements = self.__root.findall(_PATH_TO_SEQ, self.__ns)
+        return elements[0]
+
+    def __get_ns(self, prefix):
+        '''Returns namespace string.'''
+        return '{' + self.__ns[prefix] + '}'
+
+    def __repr__(self):
+        return tostring(self.__root)
 
     def __add__(self, other):
         return '%s plus %s' % (self, other)
