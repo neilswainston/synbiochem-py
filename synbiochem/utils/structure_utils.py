12ba705d5ab7f0158333854f64d58505049ccbc0
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-06-14 19:19:54 +0100

    Updated to ensure unique secondary structure for a given n-mer.

--- synbiochem/utils/structure_utils.py
+++ synbiochem/utils/structure_utils.py
@@ -7,10 +7,10 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
+# pylint: disable=too-many-arguments
 from matplotlib.colors import LinearSegmentedColormap
 import numpy
 import os
-import pylab
 import random
 import re
 import scipy.spatial
@@ -18,12 +18,14 @@ import scipy.spatial
 from Bio import SeqUtils
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Polypeptide import PPBuilder
+import pylab
 
 import synbiochem.utils.io_utils as io_utils
 
 
 _DIR = 'structure_utils'
 _PDB_DIR = 'pdb'
+_SEC_STRUCT = 'EBHIGTSL '
 
 
 def get_pdb_ids(max_ids=None, local_only=False):
@@ -50,10 +52,10 @@ def get_pdb_ids(max_ids=None, local_only=False):
         else random.sample(ids, min(len(ids), max_ids))
 
 
-def get_seq_struct(pdb_ids):
+def get_seq_structs(pdb_ids=None):
     '''Returns sequence and structure.'''
-    seq_struct = {}
-    pdb_ids = sorted(pdb_ids)
+    seq_structs = {}
+    pdb_ids = sorted(pdb_ids) if pdb_ids is not None else None
     in_field = False
     tokens = None
     str_data = ''
@@ -67,24 +69,24 @@ def get_seq_struct(pdb_ids):
             if line.startswith('>'):
                 pdb_id = re.search('(?<=\\>)[^:]*', line).group(0)
 
-                if pdb_id in pdb_ids:
+                if pdb_ids is None or pdb_id in pdb_ids:
                     if in_field:
-                        if tokens[:2] not in seq_struct:
-                            seq_struct[tokens[:2]] = [None, None]
+                        if tokens[:2] not in seq_structs:
+                            seq_structs[tokens[:2]] = [None, None]
 
-                        seq_struct[tokens[:2]][0 if tokens[2] == 'sequence'
-                                               else 1] = str_data
+                        seq_structs[tokens[:2]][0 if tokens[2] == 'sequence'
+                                                else 1] = str_data
                         str_data = ''
 
                     tokens = tuple(re.split('>|:', line.strip())[1:])
                     in_field = True
 
                 elif in_field:
-                    if tokens[:2] not in seq_struct:
-                        seq_struct[tokens[:2]] = [None, None]
+                    if tokens[:2] not in seq_structs:
+                        seq_structs[tokens[:2]] = [None, None]
 
-                    seq_struct[tokens[:2]][0 if tokens[2] == 'sequence'
-                                           else 1] = str_data
+                    seq_structs[tokens[:2]][0 if tokens[2] == 'sequence'
+                                            else 1] = str_data
                     str_data = ''
 
                     in_field = False
@@ -94,50 +96,16 @@ def get_seq_struct(pdb_ids):
             elif in_field:
                 str_data += line[:-1]
 
-    seq_struct = {key: val for key, val in seq_struct.iteritems()
-                  if val[0] is not None}
-
-    return seq_struct
+    return {key: value for key, value in seq_structs.iteritems()
+            if all(val is not None for val in value)}
 
 
-def get_pep_struct(outfilepath, length):
+def get_pep_structs(struct_set, length):
     '''Returns sequence and structure.'''
-    seq = None
-    pdb_id = None
-    chain = None
-    str_data = ''
+    for struct in struct_set:
+        assert struct in _SEC_STRUCT
 
-    source_url = 'http://www.rcsb.org/pdb/files/ss.txt'
-    target_filename = os.path.join(os.path.expanduser('~'), _DIR,
-                                   'ss.txt')
-
-    count = 0
-
-    with open(io_utils.get_file(source_url, target_filename)) as infile, \
-            open(outfilepath, 'w') as outfile:
-        for line in infile:
-            if line.startswith('>'):
-                tokens = tuple(re.split('>|:', line.strip())[1:])
-
-                if tokens[2] == 'sequence':
-                    if seq is not None:
-                        for i in range(len(seq) - length + 1):
-                            count += 1
-                            outfile.write('\t'.join([seq[i:i + length],
-                                                     str_data[i:i + length],
-                                                     pdb_id, chain,
-                                                     str(i),
-                                                     str(i + length)]) + '\n')
-                else:
-                    seq = str_data
-
-                str_data = ''
-                pdb_id = tokens[0]
-                chain = tokens[1]
-            else:
-                str_data += line[:-1]
-
-    print count
+    return [pep for struct in struct_set for pep in _get_peps(struct, length)]
 
 
 def get_sequences(pdb_id, chain=None):
@@ -225,3 +193,68 @@ def _plot(residues, values, plot_filename, plot_format, title, max_value=None):
     cbar.set_ticks([min_val, max_val])
 
     pylab.savefig(plot_filename, format=plot_format)
+
+
+def _get_peps(struct, length):
+    '''Gets n-mers of length, matching a given structure.'''
+    directory = os.path.join(os.path.expanduser('~'), _DIR, str(length))
+    filename = _get_struct_filename(directory, struct)
+
+    if not os.path.isfile(filename):
+        _write_peps(length, directory)
+
+    with open(filename) as fle:
+        return [line.split()[0] for line in fle]
+
+
+def _write_peps(length, directory):
+    '''Writes n-mers of length, matching a given structure, to file.'''
+    assert length % 2 == 1
+
+    pep_structs = {}
+
+    for seq_struct in get_seq_structs().values():
+        seq = seq_struct[0]
+        struct = seq_struct[1]
+
+        for i in range(len(seq) - length + 1):
+            pep_seq = seq[i:i + length]
+            pep_struct = struct[i:i + length]
+
+            if pep_seq not in pep_structs:
+                pep_structs[pep_seq] = set([pep_struct])
+            else:
+                pep_structs[pep_seq].add(pep_struct)
+
+    classified_peps = _get_classified_peps(pep_structs, length / 2)
+    _write_classified_peps(classified_peps, directory)
+
+
+def _get_classified_peps(pep_structs, middle_index):
+    '''Gets peptides classified by middle residue secondary structure,
+    e.g. H, S, B, etc.'''
+    classified_peps = {key: [] for key in list(_SEC_STRUCT) + ['unknown']}
+
+    for pep, structs in pep_structs.iteritems():
+        structs = list(structs)
+        clss = 'unknown' if len(structs) > 1 else structs[0][middle_index]
+        classified_peps[clss].append([pep, structs])
+
+    return classified_peps
+
+
+def _write_classified_peps(classified_peps, directory):
+    '''Writes classified peptides to file.'''
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for clss, peps in classified_peps.iteritems():
+        with open(_get_struct_filename(directory, clss), 'w+') as outfile:
+            for pep in peps:
+                outfile.write('\t'.join([str(val) for val in pep]) + '\n')
+
+
+def _get_struct_filename(directory, struct):
+    '''Gets filename for a given secondary structure class.'''
+    # struct = struct if struct is not ' ' else 'blank'
+    return os.path.join(directory, struct + '.xls')
