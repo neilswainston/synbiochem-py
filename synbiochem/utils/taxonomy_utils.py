9ce17cbb798d7cd87e8c17d921aa4eafd26fc5e0
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-09-13 07:12:08 +0100

    Adding additional organisms to PartsGenie.

---
+++ synbiochem/utils/taxonomy_utils.py
@@ -0,0 +1,27 @@
+'''
+synbiochem (c) University of Manchester 2016
+
+synbiochem is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+import json
+import urllib
+import urllib2
+
+
+def get_taxonomy_id(name):
+    '''Gets the NCBI Taxonomy id from supplied name.'''
+    url = 'http://www.ebi.ac.uk/ols/api/search?ontology=ncbitaxon' + \
+        '&exact=true&queryFields=label&q=' + urllib.quote(name)
+
+    response = urllib2.urlopen(url)
+    data = json.loads(response.read())
+
+    if data['response']['numFound'] == 1:
+        term = data['response']['docs'][0]['obo_id']
+        return term[term.find(':') + 1:]
+
+    raise RuntimeError(name + ' not found in NCBI Taxonomy')
