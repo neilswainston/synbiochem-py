c953eb0660a59d97d8cca77c67c403e9b26c47a8
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-03-06 11:08:49 +0000

    Updated plate_utils to support AssemblyGenie.

--- synbiochem/utils/plate_utils.py
+++ synbiochem/utils/plate_utils.py
@@ -7,13 +7,11 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
-import math
 
 
-def get_well_pos(pos, rows=12, columns=8):
-    '''Converts a position, pos, to a well position on a microtitre plate.'''
-    if pos >= rows * columns:
-        raise ValueError('Position ' + str(pos) + ' too large for plate.')
+def get_well(idx, rows=8, columns=12):
+    '''Map idx to well'''
+    if idx < 0 or idx >= rows * columns:
+        raise ValueError('Index %idx out of range' % idx)
 
-    return chr(int(pos % columns) + ord('A')) + \
-        str(int(math.floor(float(pos) / columns)) + 1)
+    return chr(ord('A') + (idx / rows)) + str(idx % rows + 1)
