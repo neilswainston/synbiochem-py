c3e98f2db443122373c5112e7b132bfc4d5486d7
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-04-24 14:36:31 +0100

    Updated to default by column priority.

--- synbiochem/utils/plate_utils.py
+++ synbiochem/utils/plate_utils.py
@@ -9,9 +9,24 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 '''
 
 
-def get_well(idx, rows=8, columns=12):
+def get_well(idx, rows=8, columns=12, col_ord=True):
     '''Map idx to well'''
     if idx < 0 or idx >= rows * columns:
         raise ValueError('Index %idx out of range' % idx)
 
-    return chr(ord('A') + (idx / rows)) + str(idx % rows + 1)
+    return _get_well_col(idx, rows) if col_ord \
+        else _get_well_row(idx, columns)
+
+
+def _get_well_col(idx, rows):
+    '''Map idx to well, column ordered.'''
+    return chr(ord('A') + (idx % rows)) + str(idx / rows + 1)
+
+
+def _get_well_row(idx, columns):
+    '''Map idx to well, row ordered.'''
+    return chr(ord('A') + (idx / columns)) + str(idx % columns + 1)
+
+
+for i in range(100):
+    print get_well(i, col_ord=False)
