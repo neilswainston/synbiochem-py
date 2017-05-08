6c103272e44aec581a271d89266a8a521c797dd1
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-08 16:58:22 +0100

    Moved balance code to synbiochem-py, removing glpk dependency.

--- synbiochem/utils/chem_utils.py
+++ synbiochem/utils/chem_utils.py
@@ -8,14 +8,11 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 @author:  neilswainston
 '''
 # pylint: disable=no-member
-import itertools
 import re
-
-import numpy
 
 from rdkit import Chem, DataStructs
 from rdkit.Chem import AllChem
-import synbiochem.utils.math_utils as math_utils
+import numpy
 
 
 __ELEMENTAL_MASSES = {
@@ -171,40 +168,6 @@ def parse_equation(equation, separator='='):
         _get_reaction_participants(equation_terms[1], 1)
 
 
-def balance(reaction_def, optional_comp=None, max_stoich=8.0):
-    '''Applies linear programming to balance reaction.'''
-    if optional_comp is None:
-        optional_comp = [('H', 1, 'CHEBI:24636'), ('H2O', 0, 'CHEBI:15377')]
-
-    all_formulae = [[x[0] for x in reaction_def if x[2] <= 0],
-                    [x[0] for x in reaction_def if x[2] > 0]]
-    all_formulae.extend([[x[0] for x in optional_comp] for _ in range(2)])
-
-    all_charges = [[x[1] for x in reaction_def if x[2] <= 0],
-                   [x[1] for x in reaction_def if x[2] > 0]]
-    all_charges.extend([[x[1] for x in optional_comp] for _ in range(2)])
-
-    all_ids = [[x[3] for x in reaction_def if x[2] <= 0],
-               [x[3] for x in reaction_def if x[2] > 0]]
-    all_ids.extend([[x[2] for x in optional_comp] for _ in range(2)])
-
-    all_elem_comp = [[_get_elem_comp(formula, idx)
-                      for formula in formulae]
-                     for idx, formulae in enumerate(all_formulae)]
-
-    (balanced, stoichs) = _optimise(_get_elem_matrix(all_elem_comp,
-                                                     all_charges),
-                                    max_stoich, len(optional_comp))
-
-    balanced_def = sorted(_get_reaction_def(stoichs, all_formulae,
-                                            all_charges, all_ids)) \
-        if balanced else reaction_def
-
-    return balanced, \
-        balanced and _compare_reaction_defs(reaction_def, balanced_def), \
-        balanced_def
-
-
 def get_fingerprint(smiles, radius=2):
     '''Gets a fingerprint from a SMILES.'''
     mol = Chem.MolFromSmiles(smiles)
@@ -224,73 +187,3 @@ def _get_reaction_participants(equation_term, stoich_factor):
             if len(terms) == 1
             else [terms[1], stoich_factor * float(terms[0])]
             for terms in all_terms]
-
-
-def _get_elem_comp(formula, idx):
-    '''Returns elemental compositions, multiplying stoichiometry by -1
-    if reactants.'''
-    elem_comp = get_elem_comp(formula)
-    elem_comp.update((x, y * (-1 if idx % 2 == 0 else 1))
-                     for x, y in elem_comp.items())
-    return elem_comp
-
-
-def _get_elem_matrix(all_elem_comp, all_charges):
-    '''Gets the elemental (and charge) matrix representing rows of elements
-    and columns of compounds.'''
-    a_matrix = []
-    elements = [elem_comp.keys() for elem_comps in all_elem_comp
-                for elem_comp in elem_comps]
-
-    for element in set([item for sublist in elements for item in sublist]):
-        a_matrix.append([elem_comp[element] if element in elem_comp else 0
-                         for elem_comps in all_elem_comp
-                         for elem_comp in elem_comps])
-
-    a_matrix.append(list(itertools.chain(*([charge * (-1 if idx % 2 == 0
-                                                      else 1)
-                                            for charge in charges]
-                                           for idx, charges
-                                           in enumerate(all_charges)))))
-
-    return a_matrix
-
-
-def _optimise(a_matrix, max_stoich, num_opt_formulae):
-    '''Optimise linear program and return result.'''
-    bounds = [(1.0, max_stoich)] * (len(a_matrix[0]) -
-                                    num_opt_formulae * 2) + \
-        [(0.0, max_stoich)] * num_opt_formulae * 2
-
-    return math_utils.linprog([1] * len(a_matrix[0]),
-                              a_matrix,
-                              [0.0] * (len(a_matrix)),
-                              bounds)
-
-
-def _get_reaction_def(stoichs, all_formulae, all_charges, all_ids):
-    '''Formats the input into (formula, charge, stoichiometry) tuples.'''
-    return [(a[0], b, a[1] * c, d)
-            for a, b, c, d in zip([(x, -1 if idx % 2 == 0 else 1)
-                                   for idx, formulae in enumerate(all_formulae)
-                                   for x in formulae],
-                                  [y for charges in all_charges
-                                   for y in charges],
-                                  _simplify_stoichs(stoichs),
-                                  [z for ids in all_ids for z in ids])
-            if c > 1e-8]
-
-
-def _compare_reaction_defs(def1, def2):
-    '''Returns True/False depending on whether reaction definitions are
-    equal.'''
-    return def1 is not None and def2 is not None and \
-        len(def1) == len(def2) and \
-        all([x[0] == y[0] and x[1] == y[1] and math_utils.isclose(x[2], y[2])
-             for x, y in zip(sorted(def1), sorted(def2))])
-
-
-def _simplify_stoichs(stoichs):
-    '''Attempts to simplify stoichs of 1.00000001 to 1.0.'''
-    return [float(round(stoich)) if math_utils.isclose(stoich, round(stoich))
-            else stoich for stoich in stoichs]
--- synbiochem/utils/test/test_chem_utils.py
+++ synbiochem/utils/test/test_chem_utils.py
@@ -36,47 +36,6 @@ class Test(unittest.TestCase):
         eqn = '5.6 Fe4S + -3.2 water = n+m SiO2'
         self.assertRaises(ValueError, chm_util.parse_equation, eqn)
 
-    def test_balance_unbalanced(self):
-        '''Tests get_elem_comp method for unbalanced reaction.'''
-        unbalanced = [('CO2', 0, -1.0, 'CO2'),
-                      ('C5H7O4', -1, -1.0, 'C5H7O4'),
-                      ('C3H3O3', -1, 1.0, 'C3H3O3')]
-        is_balanced, was_balanced, balanced_def = chm_util.balance(unbalanced)
-
-        self.assertTrue(is_balanced)
-        self.assertFalse(was_balanced)
-        self.assertEqual(sorted([('C3H3O3', -1, 2.0, 'C3H3O3'),
-                                 ('C5H7O4', -1, -1.0, 'C5H7O4'),
-                                 ('CO2', 0, -1.0, 'CO2'),
-                                 ('H', 1, 1.0, 'CHEBI:24636')]),
-                         sorted(balanced_def))
-
-    def test_balance_balanced(self):
-        '''Tests get_elem_comp method for balanced reaction.'''
-        balanced = [('C5H7O4', -1, -1.0, 'C5H7O4'),
-                    ('H', 1, 1.0, 'H'),
-                    ('C3H3O3', -1, 2.0, 'C3H3O3'),
-                    ('CO2', 0, -1.0, 'CO2')]
-        is_balanced, was_balanced, balanced_def = chm_util.balance(balanced)
-
-        self.assertTrue(is_balanced)
-        self.assertTrue(was_balanced)
-        self.assertEqual(sorted(balanced), sorted(balanced_def))
-
-    def test_balance_problem(self):
-        '''Tests get_elem_comp method for problematic reaction.'''
-        problem = [('C144H238N2O57P2', -2.0, -1.0, 'C144H238N2O57P2'),
-                   ('C86H142O9P', -1.0, -1.0, 'C86H142O9P'),
-                   ('H', 1.0, 1.0, 'H'),
-                   ('C150H248N2O62P2', -2.0, 1.0, 'C150H248N2O62P2'),
-                   ('C80H131O4P', -2.0, 1.0, 'C80H131O4P')]
-
-        is_balanced, was_balanced, balanced_def = chm_util.balance(
-            problem, optional_comp=[])
-
-        self.assertTrue(is_balanced)
-        self.assertTrue(was_balanced)
-        self.assertEqual(sorted(problem), sorted(balanced_def))
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
