4897d6b95397ccc9885a393fdc016cb09faa8a69
Neil Swainston <neil.swainston@manchester.ac.uk>
2018-03-02 16:47:21 +0000

    Added "human" sort method.

--- synbiochem/utils/__init__.py
+++ synbiochem/utils/__init__.py
@@ -7,8 +7,22 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
+import re
 
 
 def pairwise(iterable):
     '''s -> (s0,s1), (s1,s2), (s2, s3), ...'''
     return [(iterable[i], iterable[i + 1]) for i in range(len(iterable) - 1)]
+
+
+def sort(sortable):
+    ''''Sort the given iterable in the way that humans expect.'''
+    def convert(text):
+        '''Convert char to int if digit.'''
+        return int(text) if text.isdigit() else text
+
+    def alphanum_key(key):
+        '''Generate alphanumeric key.'''
+        return [convert(c) for c in re.split('([0-9]+)', key)]
+
+    return sorted(sortable, key=alphanum_key)
